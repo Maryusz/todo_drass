3c73c6761fe137c46b97ab5c8a170c00482af2a4
Mariusz Arnold Bilda <mariusz.mab@gmail.com>
2025-02-07 12:32:25 +0100

    Add command-based architecture to TodoViewModel and update TodoScreen UI

--- lib/ui/todo/view_model/todo_view_model.dart
+++ lib/ui/todo/view_model/todo_view_model.dart
@@ -1,6 +1,9 @@
 import 'dart:collection';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+
+import 'package:flutter_command/flutter_command.dart';
 
 import 'package:todo_drass/data/repositories/todo_repository.dart';
 import 'package:todo_drass/domain/models/todo.dart';
@@ -8,18 +11,53 @@ import 'package:todo_drass/domain/models/todo.dart';
 class TodoViewModel extends ChangeNotifier {
   TodoViewModel({
     required TodoRepository todoRepository,
-  }) : _todoRepository = todoRepository;
+  }) : _todoRepository = todoRepository {
+    // To fulfill the promise to the compiler that we will instantiate the command attribute,
+    // we intantiate it inside the constructor.
+    getTodoCommand = Command.createAsyncNoParam<void>(
+      initialValue: null,
+      () => _getTodo(),
+    );
 
+    get100TodosCommand = Command.createAsyncNoParam<void>(
+      initialValue: null,
+      () => _get100Todos(),
+    );
+  }
+
+  // The todo repository instance provided by the dependency injection container.
   final TodoRepository _todoRepository;
 
+  // The internal list of todos which are retrived from the repo.
   final List<Todo> _todos = [];
   UnmodifiableListView<Todo> get todos => UnmodifiableListView(_todos);
 
-  Future<void> getTodo() async {
+  // Property
+  int get todosCount => _todos.length;
+
+  // All the commands that can be executed by the view model.
+  late final Command getTodoCommand;
+  late final Command get100TodosCommand;
+
+  Future<void> _getTodo() async {
+    int i = Random().nextInt(100);
+    if (i > 80) {
+      throw Exception('Random Exception');
+    }
+
     final todo = await _todoRepository.getTodo();
     _todos.add(todo);
     notifyListeners();
   }
 
-  // Add your ViewModel code here
+  Future<void> _get100Todos() async {
+    for (int i = 0; i < 100; i++) {
+      _todos.add(await _todoRepository.getTodo());
+      // uncomment to see the progress
+      // notifyListeners();
+    }
+
+    // this updates it at the end
+    notifyListeners();
+  }
 }
--- lib/ui/todo/widgets/todo_screen.dart
+++ lib/ui/todo/widgets/todo_screen.dart
@@ -11,28 +11,74 @@ class TodoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
-      ),
-      body: ListenableBuilder(
-        listenable: viewModel,
-        builder: (context, child) {
-          return ListView.builder(
-            itemCount: viewModel.todos.length,
-            itemBuilder: (context, index) {
-              final todo = viewModel.todos[index];
-              return ListTile(
-                title: Text(todo.title),
-                subtitle: Text(todo.description),
+        title: ListenableBuilder(
+            listenable: viewModel,
+            builder: (context, _) {
+              return Badge.count(
+                count: viewModel.todosCount,
+                child: const Text('Todo List'),
               );
+            }),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              viewModel.get100TodosCommand.execute();
             },
-          );
-        },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            width: 400,
+            child: Center(
+              child: ValueListenableBuilder(
+                valueListenable: viewModel.get100TodosCommand.isExecuting,
+                builder: (context, v, _) {
+                  return v ? LinearProgressIndicator() : Text("Press the refresh button to get 100 todos");
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListenableBuilder(
+              listenable: viewModel,
+              builder: (context, child) {
+                return ListView.builder(
+                  itemCount: viewModel.todos.length,
+                  itemBuilder: (context, index) {
+                    final todo = viewModel.todos[index];
+                    return ListTile(
+                      title: Text(todo.title),
+                      subtitle: Text(todo.description),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await viewModel.getTodo();
+        onPressed: () {
+          viewModel.getTodoCommand.execute();
+
+          viewModel.getTodoCommand.errors.addListener(() {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("An error occurred!"),
+              ),
+            );
+          });
         },
-        child: const Icon(Icons.add),
+        child: ValueListenableBuilder(
+          valueListenable: viewModel.getTodoCommand.isExecuting,
+          builder: (context, v, _) {
+            return v ? CircularProgressIndicator() : Icon(Icons.add);
+          },
+        ),
       ),
     );
   }
